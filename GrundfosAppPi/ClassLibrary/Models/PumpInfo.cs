954e0fcd442f83b829231ed424c7e1817e93d627
Dragonslayer420 <simonsondergaardchristiansen@gmail.com>
2021-05-19 11:46:23 +0200

    CKewsl

---
+++ GrundfosAppPi/ClassLibrary/Models/PumpInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Models
+{
+    public class PumpInfo
+    {
+        public int Frequence { get; set; }
+        public double PowerUsage { get; set; }
+        public double Flow { get; set; }
+
+        public PumpInfo(int frequence, double powerUsage, double flow)
+        {
+            Frequence = frequence;
+            PowerUsage = powerUsage;
+            Flow = flow;
+        }
+
+        public override string ToString()
+        {
+            return Frequence + " " + PowerUsage + " " + Flow;
+        }
+    }
+}
---
+++ GrundfosAppPi/PumpEmulator/Program.cs
@@ -0,0 +1,36 @@
+﻿using ClassLibrary.Models;
+using System;
+using System.Threading;
+
+namespace PumpEmulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainFlow();
+
+        }
+
+        private static void MainFlow()
+        {
+            Pump thisPump = new Pump("ALPHA3 Model B");
+
+            while (thisPump.TurnedOn)
+            {
+               PumpInfo pumpData = thisPump.GetValues();
+
+                Console.WriteLine(pumpData);
+
+
+                SendToIoTHub();
+                Thread.Sleep(15000);
+            }
+        }
+
+        private static void SendToIoTHub()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
