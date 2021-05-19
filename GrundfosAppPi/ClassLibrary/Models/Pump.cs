3d41a74edc0a164b2bfcd5b48f8a7205748ebb98
Frozt <51519573+FroztOfficial@users.noreply.github.com>
2021-05-19 10:26:04 +0200

    Recreated projects and added UI.

---
+++ GrundfosAppPi/ClassLibrary/Models/Pump.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Models
+{
+    public class Pump
+    {
+        public int Speed { get; set; }
+        public string ControlMode { get; set; }
+        public string OperatingMode { get; set; }
+        public int Setpoint { get; set; }
+    }
+}
