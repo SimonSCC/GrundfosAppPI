21224537808e7782cb4c2608f5fe804a9baf1fe5
Dragonslayer420 <simonsondergaardchristiansen@gmail.com>
2021-05-24 11:55:25 +0200

    Flyttet noget logik fra frontend ind i modellen til PumpInfo

--- GrundfosAppPi/ClassLibrary/Models/PumpInfo.cs
+++ GrundfosAppPi/ClassLibrary/Models/PumpInfo.cs
@@ -11,10 +11,95 @@ namespace ClassLibrary.Models
     public class PumpInfo
     {
         public string Name { get; set; }
-        public int Frequence { get; set; }
         public double PowerUsage { get; set; }
-        public double Flow { get; set; }
-        public int Speed { get; set; }
+
+
+        private int frequence;
+        public int Frequence
+        {
+            get { return frequence; }
+            set
+            {
+                if (value <= 10 || value >= 55)
+                {
+                    FrequenceStatus = StatusCode.Warning;
+
+                    if (value <= 5 || value >= 60)
+                    {
+                        FrequenceStatus = StatusCode.Danger;
+                    }
+                }
+                else
+                {
+                    FrequenceStatus = StatusCode.Good;
+                }
+
+                frequence = value;
+            }
+        }
+
+        public StatusCode FrequenceStatus { get; set; }
+
+
+
+        private double flow;
+        public double Flow
+        {
+            get { return flow; }
+            set
+            {
+                if (value <= 4 || value >= 12)
+                {
+                    FlowStatus = StatusCode.Warning;
+
+                    if (value <= 0 || value >= 16)
+                    {
+                        FlowStatus = StatusCode.Danger;
+                    }
+                }
+                else
+                {
+                    FlowStatus = StatusCode.Good;
+                }
+
+                flow = value;
+            }
+        }
+
+        public StatusCode FlowStatus { get; set; }
+
+
+
+
+        private int speed;
+
+        public int Speed
+        {
+            get { return speed; }
+            set
+            {
+                if (value <= 150 || value >= 1850)
+                {
+                    SpeedStatus = StatusCode.Warning;
+
+                    if (value <= 100 || value >= 1900)
+                    {
+                        SpeedStatus = StatusCode.Danger;
+                    }
+                }
+                else
+                {
+                    SpeedStatus = StatusCode.Good;
+                }
+
+                speed = value;
+            }
+        }
+
+        public StatusCode SpeedStatus { get; set; }
+
+
+
 
         public PumpInfo(string name, int frequence, double powerUsage, double flow, int speed)
         {
@@ -34,5 +119,12 @@ namespace ClassLibrary.Models
         {
             return JsonSerializer.Serialize(this);
         }
+    }
+
+    public enum StatusCode
+    {
+        Good,
+        Warning,
+        Danger,
     }
 }
